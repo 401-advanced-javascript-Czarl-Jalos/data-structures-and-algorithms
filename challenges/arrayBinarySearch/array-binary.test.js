626347ee22a3dbe5df2a5526a0b7d83273ce22f6
Czarl Jalos <czarlfrancis@gmail.com>
2020-11-03 13:44:33 -0800

    solved CC-03

---
+++ challenges/arrayBinarySearch/array-binary.test.js
@@ -0,0 +1,35 @@
+
+const binarySearch = require('./array-binary-search.js');
+
+describe('testing the binarySearch function', () => {
+
+  it('should not modify the original array', () => {
+    const startArray = [10, 12, 16, 21, 35];
+    expect(startArray.length).toBe(5);
+  });
+  
+  it('returns an index if number exists within an array with an odd number of elements', () => {
+    const startArray = [10, 12, 16, 21, 35];
+    expect(binarySearch(startArray, 21)).toBe(3);
+  });
+
+  it('returns an index if number exists within an array with an even number of elements', () => {
+    const startArray = [10, 12, 16, 21, 35, 68];
+    expect(binarySearch(startArray, 12)).toBe(1);
+  });
+
+  it('returns -1 if number does not exist within an odd-numbered array', () => {
+    const startArray = [10, 12, 16, 21, 35, 68, 89];
+    expect(binarySearch(startArray, 77)).toBe(-1);
+  });
+
+  it('returns -1 if number does not exist within an even-numbered array', () => {
+    const startArray = [10, 12, 16, 35, 68, 89];
+    expect(binarySearch(startArray, 77)).toBe(-1);
+  });
+
+  it('returns an index in an array of two elements', () => {
+    const startArray = [10, 12];
+    expect(binarySearch(startArray, 12)).toBe(1);
+  });
+});
